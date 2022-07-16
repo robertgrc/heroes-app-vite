0f0e0047c26545d382fd12b6b2309ce5c752d1b2
robertgrc <robert.rodriguez.rgrc@gmail.com>
2022-07-16 13:16:18 -0400

    191. Navigate push / replace - useNavigate

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,8 +1,27 @@
+import {useNavigate } from 'react-router-dom';
+
 import React from 'react'
 
 export const LoginPage = () => {
+
+const navigate = useNavigate()
+const onLogin = () =>{
+  navigate("/dc", {
+    replace: true
+  })
+}
+
   return (
-   <h1>LoginPage</h1>
+   <div className='container mt-5'>
+    <h1>Login</h1>
+    <hr />
+    <button 
+        className='btn btn-primary'
+        onClick={onLogin}
+        >
+      Login
+    </button>
+   </div>
   )
 }
 
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route} from "react-router-dom";
 import { LoginPage } from "../auth";
 import { HeroesRoutes} from "../heroes";
-import { Navbar } from "../ui";
+
 
 export const AppRouter = () => {
   return (
